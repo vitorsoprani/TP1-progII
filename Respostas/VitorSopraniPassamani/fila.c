25ac7ee6c2b8125c4aaab7c04a9254bc1d0eb96b
vitorsoprani <vitor.spassamani@gmail.com>
2025-02-27 19:38:33 -0300

    Implementação fila.c

--- Respostas/VitorSopraniPassamani/fila.c
+++ Respostas/VitorSopraniPassamani/fila.c
@@ -1,70 +1,134 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include "fila.h"
+
+#define DEBUG_FILA 1
+
+struct Fila {
+    void** tickets;//Vetor de ponteiros que armazena os tickets da fila.
+    int qtdTickets;//Quantidade de tickets atualmente na fila.
+
+};
+
+Fila *criaFila() {
+    Fila* f = (Fila*)malloc(sizeof(Fila));
+
+    #if DEBUG_FILA
+        assert(f != NULL);
+    #else
+        if (f == NULL)
+            return f;
+    #endif
+
+    //  A fila inicia vaza, o vetor 'tickets' é alocado com tamanho 0 para ser realocado na medida em que os tickets vao entrando.
+    f->qtdTickets = 0;
+    f->tickets = (void**)malloc(0);
+
+    return f;
+}
+
+void desalocaFila(Fila *f) {
+    if (f != NULL) {
+        for (int i = 0; i < f->qtdTickets; i++)
+            desalocaTicket(f->tickets[i]);
+        
+        free(f->tickets);
+        free(f);
+    }
+}
+
+void insereTicketFila(Fila *f, char *cpfSol, void *dado, func_ptr_tempoEstimado getTempo, func_ptr_tipo getTipo, func_ptr_notifica notifica, func_ptr_desaloca desaloca) {
+    #if DEBUG_FILA
+    assert(f != NULL);
+    assert(dado != NULL);
+    #else
+        if (f == NULL || dado == NULL)
+            return;
+    #endif
+    
+    Ticket* t = criaTicket(cpfSol, dado, getTempo, getTipo, notifica, desaloca);
+
+    #if DEBUG_FILA
+        assert(t != NULL);
+    #else
+        if (t == NULL)
+            return;
+    #endif
+    
+    
+    //  Manipulando a memoria para comportar o novo ticket
+    f->qtdTickets++;
+    f->tickets = (void**)realloc(f->tickets, sizeof(void*) * f->qtdTickets);
+    
+    #if DEBUG_FILA
+        assert(f->tickets != NULL);
+    #else
+        if (f->tickets == NULL)
+            return f;
+    #endif
+
+    f->tickets[f->qtdTickets - 1] = t;
+
+    char id[MAX_TAM_ID] = "";
+    sprintf(id, "Tick-%d", f->qtdTickets);
+    setIDTicket(t, id);
+}
+
+int getQtdTicketsNaFila(Fila *f) {
+    #if DEBUG_FILA
+        assert(f != NULL);
+    #else
+        if (f == NULL)
+            return 0;
+    #endif
+
+    return f->qtdTickets;
+}
+
+int getQtdTicketsPorStatusNaFila(Fila *f, char status) {
+    #if DEBUG_FILA
+        assert(f != NULL);
+    #else
+        if (f == NULL)
+            return 0;
+    #endif
 
 
-#ifndef _FILA_H
-#define _FILA_H
+    int qtd = 0;
 
-#include "ticket.h"
-/**
- * Uma estrutura que armazenará os tickets que serão atribuidos aos técnicos.
- *
- * A estrutura seguirá a ideia de uma Fila, isto é, primeiro ticket deve ser o primeiro a ser tratado.
- */
-typedef struct Fila Fila;
+    for (int i = 0; i < f->qtdTickets; i++) {
+        if (getStatusTicket(f->tickets[i]) == status)
+            qtd++;
+    }
 
-/**
- * @brief Aloca uma estrutura Fila na memória e inicializa os parâmetro necessários
- * @return  Uma Fila inicializada.
- */
-Fila *criaFila();
+    return qtd;
+}
 
-/**
- * @brief  Desaloca uma fila da memória
- * @param f estrutura do tipo Fila que deve ser liberada da memória
- * @return (void)
- */
-void desalocaFila(Fila *f);
+Ticket *getTicketNaFila(Fila *f, int i) {
+    #if DEBUG_FILA
+        assert(f != NULL);
+        assert(i < f->qtdTickets && i >= 0);
+    #else
+        if (f == NULL)
+            return NULL;
+        if (i >= f->qtdTickets || i < 0)
+            return NULL;
+    #endif
 
-/**
- * @brief  Insere um ticket na fila de processamento. Um ticket deve ser inserido sempre na última posição.
- * Obviamente, essa função também faz a manipulação de memória necessária para alocar um novo ticket.
- * @param f  Fila que receberá o novo ticket
- * @param cpfSol CPF de quem está solicitando a abertura do ticket
- * @param dado   Um ticket genérico  (considerando que existe mais de um tipo de ticket)
- * @param getTempo  Função de callback que retorna o tempo para aquele ticket ser resolvido (ver ticket.h)
- * @param getTipo   Função de callback que retorna o tipo do ticket ser resolvido (ver ticket.h)
- * @param notifica  Função de callback de notificação de um ticket (ver ticket.h)
- * @param desaloca  Função de callback que desaloca  um ticket da memória (ver ticket.h)
- */
-void insereTicketFila(Fila *f, char *cpfSol, void *dado, func_ptr_tempoEstimado getTempo,
-                      func_ptr_tipo getTipo, func_ptr_notifica notifica, func_ptr_desaloca desaloca);
+    return f->tickets[i];
+}
 
-/**
- * @brief Recupera a quantidade de tickets  em uma fila
- * @param f  Estrutura Fila inicializada.
- * @return Quantidade de Tickets na fila
- */
-int getQtdTicketsNaFila(Fila *f);
+void notificaFila(Fila *f) {
+    #if DEBUG_FILA
+        assert(f != NULL);
+    #else
+        if (fila == NULL)
+            return;
+    #endif
 
-/**
- * @brief Recupera a quantidade de tickets em uma fila com um determinado status
- * @param f  Estrutura Fila inicializada.
- * @param status Status do ticket
- * @return Quantidade de Tickets na fila com o status informado
- */
-int getQtdTicketsPorStatusNaFila(Fila *f, char status);
-
-/**
- * @brief Recupera um ticket na fila de processamento. Um ticket deve ser recuperado sempre na i-ésima posição.
- * @param f  Fila que contém o ticket
- * @param i  Posição do ticket na fila
- * @return  Ticket recuperado da fila
- */
-Ticket *getTicketNaFila(Fila *f, int i);
-
-/**
- * @brief A função notificaFila imprime todos os tickets na Fila f
- * @param f  Fila inicializada contendo zero ou mais tickets.
- */
-void notificaFila(Fila *f);
-
-#endif
+    for (int i = 0; i < f->qtdTickets; i++) {
+        notificaTicket(((Ticket*)f->tickets[i]));
+    }
+}
